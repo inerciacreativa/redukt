ee307d0dae2e65ecbedaf5296a6b60e677a46193
Jose Cuesta <jose.cuesta@inerciacreativa.com>
2021-01-31 20:34:59 +0100

    Clean output

--- bin/build.js
+++ bin/build.js
@@ -2,8 +2,7 @@
 
 const {argv} = require('yargs');
 const {spawn} = require('child_process');
-const output = require('./../lib/helpers/output');
-const params = ['npx webpack', '--config=node_modules/redukt/lib/webpack.config.js', '--progress'];
+const params = ['webpack', '--config=node_modules/redukt/lib/webpack.config.js', '--progress'];
 
 if (argv.p || argv.production) {
 	params.push('--mode=production')
@@ -13,27 +12,3 @@ const webpack = spawn(params.join(' '), {
 	stdio: 'inherit',
 	shell: true
 });
-let clear = null;
-let date = false;
-/*
-webpack.stdout.on('data', (buffer) => {
-	const text = output.parseBuffer(buffer, date);
-
-	output.write(text, clear);
-
-	date = true;
-	if (clear === true) {
-		clear = false;
-	}
-})
-
-webpack.stderr.on('data', (buffer) => {
-	const text = output.parseError(buffer);
-
-	if (clear === null) {
-		clear = true;
-	}
-
-	text.forEach(line => output.write(line, clear));
-});
-*/
